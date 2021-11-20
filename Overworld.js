1b6c84fd9abb08aa27877e7d3bbe7673e4f9a31a
featherwalk <vacantorange@gmail.com>
2021-11-19 22:50:46 -0600

    finished second video| started gameobject and sprite classes

---
+++ Overworld.js
@@ -0,0 +1,33 @@
+class Overworld {
+  constructor(config) {
+    this.element = config.element;
+    this.canvas = this.element.querySelector(".game-canvas");
+    this.ctx = this.canvas.getContext("2d");
+  }
+
+  init() {
+    console.log("hello from Overworld", this);
+    const image = new Image();
+    image.onload = () => {
+      this.ctx.drawImage(image, 0, 0);
+    };
+    image.src = "/images/maps/DemoLower.png";
+
+    // Place game objects
+
+    const hero = new GameObject({
+      x: 5,
+      y: 6,
+    });
+    const npc1 = new GameObject({
+      x: 7,
+      y: 9,
+      src: "/images/characters/people/npc1.png",
+    });
+
+    setTimeout(() => {
+      hero.sprite.draw(this.ctx);
+      npc1.sprite.draw(this.ctx);
+    }, 200);
+  }
+}
