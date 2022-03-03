0398c506c31495af9c44a7f96579ee5b9f3da6eb
featherwalk <vacantorange@gmail.com>
2022-03-02 21:43:35 -0600

    created levels, recipes, and ingredients content lists

---
+++ Content/recipes.js
@@ -0,0 +1,99 @@
+window.recipes = {
+  n001: {
+    label: "Slice Samurai",
+    ingredients: ["pepperoni", "cheddar", "tomato sauce"],
+  },
+  n002: {
+    label: "Alfredo Snake-eyes",
+    ingredients: ["black olives", "cheddar", "alfredo sauce"],
+  },
+  s001: {
+    label: "Bacon Brigade",
+    ingredients: ["bacon", "feta", "spicy marinara"],
+  },
+  s002: {
+    label: "Deep Dish Volcano",
+    ingredients: ["four cheese mix", "peppers", "spicy marinara"],
+  },
+  v001: {
+    label: "Kale-eye-descope",
+    ingredients: ["kale", "cheddar", "tomato sauce"],
+  },
+  v002: {
+    label: "Spinning Spinach",
+    ingredients: ["spinach", "feta", "alfredo sauce"],
+  },
+  f001: {
+    label: "Portobello Express",
+    ingredients: ["portobello", "cheddar", "tomato sauce"],
+  },
+  f002: {
+    label: "Chanterella",
+    ingredients: ["Chanterelle", "feta", "garlic sauce"],
+  },
+  c001: {
+    label: "Pineapple Powerhouse",
+    ingredients: ["pineapple", "cheddar", "alfredo sauce"],
+  },
+  c002: {
+    label: "Margherita Mellow",
+    ingredients: ["tomatoes", "basil", "alfredo sauce"],
+  },
+};
+
+window.ingredients = {
+  pepperoni: {
+    price: 37,
+  },
+  cheddar: {
+    price: 20,
+  },
+  "tomato sauce": {
+    price: 25,
+  },
+  "black olives": {
+    price: 15,
+  },
+  "alfredo sauce": {
+    price: 20,
+  },
+  bacon: {
+    price: 45,
+  },
+  feta: {
+    price: 25,
+  },
+  "spicy marinara": {
+    price: 30,
+  },
+  "four cheese mix": {
+    price: 40,
+  },
+  peppers: {
+    price: 20,
+  },
+  kale: {
+    price: 25,
+  },
+  spinach: {
+    price: 30,
+  },
+  portobello: {
+    price: 25,
+  },
+  chanterelle: {
+    price: 35,
+  },
+  "garlic sauce": {
+    price: 25,
+  },
+  pineapple: {
+    price: 35,
+  },
+  tomatoes: {
+    price: 30,
+  },
+  basil: {
+    price: 15,
+  },
+};
