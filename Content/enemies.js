dc2e1142665884874c8a00b36825e87b1e8d4f2f
featherwalk <vacantorange@gmail.com>
2022-03-06 12:37:13 -0600

    playtest changes

--- Content/enemies.js
+++ Content/enemies.js
@@ -20,7 +20,7 @@ window.enemies = {
     src: "/images/characters/people/npc1.png",
     pizzas: {
       a: {
-        hp: 1,
+        hp: 280,
         pizzaId: "f001",
         maxHp: 280,
         level: 1,
