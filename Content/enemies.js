896dd27f5e6f44d51597f9ee859617e814ef0cd9
featherwalk <vacantorange@gmail.com>
2022-03-06 21:42:12 -0600

    added rewards to enemies

--- Content/enemies.js
+++ Content/enemies.js
@@ -2,6 +2,10 @@ window.enemies = {
   erio: {
     name: "Erio",
     src: "/images/characters/people/erio.png",
+    rewards: {
+      currency: 1000,
+      items: ["item_recoverHp2"],
+    },
     pizzas: {
       a: {
         pizzaId: "s001",
@@ -18,6 +22,10 @@ window.enemies = {
   beth: {
     name: "Beth",
     src: "/images/characters/people/npc1.png",
+    rewards: {
+      currency: 100,
+      items: ["item_recoverHp"],
+    },
     pizzas: {
       a: {
         hp: 2,
